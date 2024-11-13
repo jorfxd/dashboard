3edc2e86322bcad2cd623d645c623d3dd484dd2b
Cliente CONTROL-PC <CltControl@GGIECLUGWRK177.espol.edu.ec>
2024-11-13 14:15:50 -0500

    actividad 11
    actividad 11

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Grid from '@mui/material/Grid2'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <Grid container spacing={5}>
+
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+
+        <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
+
+        <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
+       
+    </Grid>
+)
+}
+
+export default App
