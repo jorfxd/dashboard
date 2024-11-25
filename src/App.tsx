d9e1a2dfc2d7b09b796cbb1e0e000ddf80584b29
jorfxd <149507738+jorfxd@users.noreply.github.com>
2024-11-25 13:54:20 -0500

    Actividadad 11
    De esa actividad primer versionamiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
-import Grid from '@mui/material/Grid2'
-
+ // Grid version 2
+ import Grid from '@mui/material/Grid2' 
 function App() {
-  const [] = useState(0)
+  const [count, setCount] = useState(0)
+
   return (
-    <Grid container spacing={5}>
+    <Grid>
 
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
-
-        <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
-
-        <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
+        {/* Indicadores */}
+        <Grid>Elemento: Indicador 1</Grid>
+        <Grid>Elemento: Indicador 2</Grid>
+        <Grid>Elemento: Indicador 3</Grid>
+        <Grid>Elemento: Indicador 4</Grid>
        
+        {/* Tabla */}
+        <Grid>Elemento: Tabla</Grid>
+       
+        {/* Gráfico */}
+        <Grid>Elemento: Gráfico 1</Grid>
+   
     </Grid>
-)
+  )
 }
 
 export default App
---
+++ src/components/IndicatorWeather.tsx
@@ -0,0 +1,8 @@
+
+export default function IndicatorWeather() {
+    return (
+        <>
+            Componente IndicatorWeather
+        </> 
+    )
+}
