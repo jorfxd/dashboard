2b7a752916af36e324ddfd42fbc3594cdb19c282
jorfxd <149507738+jorfxd@users.noreply.github.com>
2024-11-25 14:48:02 -0500

    corrigiendo errores
    olaaa

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,16 @@ function App() {
     <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1<IndicatorWeather />
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather />
         <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /> 
         </Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2<IndicatorWeather />
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather />
         <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
         </Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3<IndicatorWeather />
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather />
         <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
         </Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4<IndicatorWeather />
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather />
         <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
         </Grid>
 
@@ -44,7 +44,7 @@ function App() {
              
 
         {/* Gráfico */}
-        <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
+        <Grid size={{ xs: 12, xl: 4 }}></Grid>
        
     </Grid>
 )
