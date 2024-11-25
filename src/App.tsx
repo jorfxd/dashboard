fe24c822548eec5e7071fc22d7fde7b1fa21112f
jorfxd <149507738+jorfxd@users.noreply.github.com>
2024-11-25 14:14:47 -0500

    Actividad12
    commit1 actividad esta

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 
+import IndicatorWeather from './components/IndicatorWeather';
  // Grid version 2
  import Grid from '@mui/material/Grid2' 
 
@@ -9,10 +10,10 @@ function App() {
     <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1<IndicatorWeather /></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2<IndicatorWeather /></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3<IndicatorWeather /></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4<IndicatorWeather /></Grid>
 
         {/* Tabla */}
         <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
